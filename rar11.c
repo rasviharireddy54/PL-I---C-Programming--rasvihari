e8fe74b1675dab77ed65ae6d7d9ef4b43a49d2e7
Rasvihari Reddy <rasviharireddy@gmail.com>
2026-09-14 16:01:21 +0530

    Add files via upload

---
+++ rar11.c
@@ -0,0 +1,29 @@
+// Program(11)-> Write a program to check whether a given number is even or odd
+#include<stdio.h>
+
+int main()
+ 
+{   
+    int num;
+     printf("Enter your Number:");
+     scanf("%d",&num);
+     
+     if (num % 2 ==0)
+    
+     {
+      printf("The number is Even");
+        }
+
+      else
+      
+      {
+       printf("The number is Odd");
+      }
+       return 0;
+
+}
+    
+         
+
+       
+    
